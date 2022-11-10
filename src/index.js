3a7c480d7739accd96ab0cebcf3733b52026eec6
Chance <jchancehud@gmail.com>
2022-11-10 09:33:39 -0600

    feat: base64 constants

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ function unstringifyBigInts(o) {
         return BigInt(o);
     } else if (typeof o == "string" && /^0x[0-9a-fA-F]+$/.test(o)) {
         return BigInt(o);
+    } else if (typeof o == 'string' && /^[a-zA-Z0-9+\/]+={0,2}$/.test(o)) {
+        // base64
+        const byteArray = Uint8Array.from(atob(o), c => c.charCodeAt(0))
+        const hex = [...byteArray].map(x => x.toString(16).padStart(2, '0')).join('')
+        return BigInt(`0x${hex}`)
     } else if (Array.isArray(o)) {
         return o.map(unstringifyBigInts);
     } else if (typeof o == "object") {
