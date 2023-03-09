63354748dd767fa32cfb55db640269763fb0ae2d
Chance <jchancehud@gmail.com>
2023-03-09 13:39:00 -0600

    chore: update typings

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,114 @@
 declare module 'poseidon-lite' {
-  function poseidon(input: bigint[]): bigint
-  export = poseidon
+  export function poseidon1(input: bigint[]): bigint
+  export function poseidon2(input: bigint[]): bigint
+  export function poseidon3(input: bigint[]): bigint
+  export function poseidon4(input: bigint[]): bigint
+  export function poseidon5(input: bigint[]): bigint
+  export function poseidon6(input: bigint[]): bigint
+  export function poseidon7(input: bigint[]): bigint
+  export function poseidon8(input: bigint[]): bigint
+  export function poseidon9(input: bigint[]): bigint
+  export function poseidon10(input: bigint[]): bigint
+  export function poseidon11(input: bigint[]): bigint
+  export function poseidon12(input: bigint[]): bigint
+  export function poseidon13(input: bigint[]): bigint
+  export function poseidon14(input: bigint[]): bigint
+  export function poseidon15(input: bigint[]): bigint
+  export function poseidon16(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon1' {
+  export function poseidon1(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon2' {
+  export function poseidon2(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon3' {
+  export function poseidon3(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon4' {
+  export function poseidon4(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon5' {
+  export function poseidon5(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon6' {
+  export function poseidon6(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon7' {
+  export function poseidon7(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon8' {
+  export function poseidon8(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon9' {
+  export function poseidon9(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon10' {
+  export function poseidon10(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon11' {
+  export function poseidon11(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon12' {
+  export function poseidon12(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon13' {
+  export function poseidon13(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon14' {
+  export function poseidon14(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon15' {
+  export function poseidon15(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/poseidon16' {
+  export function poseidon16(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon1' {
+  export function poseidon1(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon2' {
+  export function poseidon2(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon3' {
+  export function poseidon3(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon4' {
+  export function poseidon4(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon5' {
+  export function poseidon5(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon6' {
+  export function poseidon6(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon7' {
+  export function poseidon7(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon8' {
+  export function poseidon8(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon9' {
+  export function poseidon9(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon10' {
+  export function poseidon10(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon11' {
+  export function poseidon11(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon12' {
+  export function poseidon12(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon13' {
+  export function poseidon13(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon14' {
+  export function poseidon14(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon15' {
+  export function poseidon15(input: bigint[]): bigint
+}
+declare module 'poseidon-lite/opt/poseidon16' {
+  export function poseidon16(input: bigint[]): bigint
 }
