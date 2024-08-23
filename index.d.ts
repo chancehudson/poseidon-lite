19b785efffcbd31e32281e1692b48fd4590b4eba
vimwitch <jchancehud@gmail.com>
2024-08-22 20:43:17 -0700

    chore: update index typings

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,18 @@
 declare module 'poseidon-lite' {
-  export function poseidon1(input: (bigint | number | string)[]): bigint
-  export function poseidon2(input: (bigint | number | string)[]): bigint
-  export function poseidon3(input: (bigint | number | string)[]): bigint
-  export function poseidon4(input: (bigint | number | string)[]): bigint
-  export function poseidon5(input: (bigint | number | string)[]): bigint
-  export function poseidon6(input: (bigint | number | string)[]): bigint
-  export function poseidon7(input: (bigint | number | string)[]): bigint
-  export function poseidon8(input: (bigint | number | string)[]): bigint
-  export function poseidon9(input: (bigint | number | string)[]): bigint
-  export function poseidon10(input: (bigint | number | string)[]): bigint
-  export function poseidon11(input: (bigint | number | string)[]): bigint
-  export function poseidon12(input: (bigint | number | string)[]): bigint
-  export function poseidon13(input: (bigint | number | string)[]): bigint
-  export function poseidon14(input: (bigint | number | string)[]): bigint
-  export function poseidon15(input: (bigint | number | string)[]): bigint
-  export function poseidon16(input: (bigint | number | string)[]): bigint
+  export function poseidon1(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon2(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon3(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon4(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon5(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon6(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon7(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon8(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon9(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon10(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon11(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon12(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon13(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon14(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon15(input: (bigint | number | string)[], nOuts?: number): bigint
+  export function poseidon16(input: (bigint | number | string)[], nOuts?: number): bigint
 }
